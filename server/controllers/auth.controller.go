ec497e54614326eef1dbc93564f14f75a62d759d
NadimRifaii <nadimrifaii3@gmail.com>
2023-12-27 12:42:20 +0200

    implemented the loger function that returns a response to the client

--- server/controllers/auth.controller.go
+++ server/controllers/auth.controller.go
@@ -1,7 +1,12 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 func Signup(c *fiber.Ctx) error {
 
+}
+func loger(c *fiber.Ctx, status int, m fiber.Map) error {
+	return c.Status(status).JSON(m)
 }
