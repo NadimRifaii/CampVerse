6a4f2793401e55cecb7bda2a607b7327064a1621
NadimRifaii <nadimrifaii3@gmail.com>
2023-12-27 11:05:51 +0200

    initialized the fiber app and started the server

--- server/server.go
+++ server/server.go
@@ -1,10 +1,16 @@
 package main
 
-import "github.com/NadimRifaii/campverse/initializers"
+import (
+	"os"
+
+	"github.com/NadimRifaii/campverse/initializers"
+	"github.com/gofiber/fiber/v2"
+)
 
 func init() {
 	initializers.Load()
 }
 func main() {
-
+	app := fiber.New()
+	app.Listen(":" + os.Getenv("PORT"))
 }
