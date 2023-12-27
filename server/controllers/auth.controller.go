2068552ce6832170333a490d2ae9fb98eccf6316
NadimRifaii <nadimrifaii3@gmail.com>
2023-12-27 13:44:36 +0200

    implemented isDuplicateKey function

--- server/controllers/auth.controller.go
+++ server/controllers/auth.controller.go
@@ -6,19 +6,46 @@ import (
 
 	"github.com/NadimRifaii/campverse/database"
 	"github.com/NadimRifaii/campverse/models"
+	"github.com/go-sql-driver/mysql"
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v4"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func Signup(c *fiber.Ctx) error {
+	user := new(models.User)
+	userRole := new(models.UserRole)
+	if err := validateUserRequest(c, user, userRole); err != nil {
+		return loger(c, fiber.StatusBadRequest, fiber.Map{"error": err.Error()})
+	}
+	tokenString := createJwtToken(user, userRole.RoleName)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+	if err != nil {
+		return loger(c, fiber.StatusInternalServerError, fiber.Map{"error": "Internal server error"})
+	}
+	user.Password = string(hashedPassword)
+	err = createUserInDb(c, user)
 
 }
-func validateUserRequest(c *fiber.Ctx, user *models.User) error {
-	if err := c.BodyParser(user); err != nil {
+func validateUserRequest(c *fiber.Ctx, user *models.User, userRole *models.UserRole) error {
+	var body struct {
+		Username string `json:"username" gorm:"not null;default:'first';size:255"`
+		Lastname string `json:"lastname" gorm:"not null;default:'last';size:255"`
+		Email    string `json:"email" gorm:"not null;size:255;unique"`
+		Password string `json:"password" gorm:"not null;size:255"`
+		RoleName string `json:"role" gorm:"not null;default:'user';size:255" `
+	}
+	if err := c.BodyParser(body); err != nil {
 		return errors.New("invalid request body")
-	} else if user.Email == "" || user.Password == "" {
+	} else if body.Email == "" || body.Password == "" || body.RoleName == "" {
 		return errors.New("missing credentials")
 	}
+	getRoleId(userRole, body.RoleName)
+	user.Email = body.Email
+	user.Password = body.Password
+	user.RoleID = userRole.ID
+	user.Username = body.Username
+	user.Lastname = body.Lastname
 	return nil
 }
 
@@ -34,17 +61,23 @@ func createUserInDb(c *fiber.Ctx, user *models.User) error {
 	return nil
 }
 func createJwtToken(user *models.User, roleName string) *jwt.Token {
-	userRole := getRoleId(roleName)
 	return jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"exp":      time.Now().Add(time.Hour).Unix(),
-		"role":     userRole.ID,
+		"role":     roleName,
 		"username": user.Username,
 		"email":    user.Email,
 	})
 }
-func getRoleId(roleName string) *models.UserRole {
-	userRole := new(models.UserRole)
+func getRoleId(userRole *models.UserRole, roleName string) {
 	db := database.Db
 	userRole.GetIDByRoleName(db, roleName)
-	return userRole
+}
+func isDuplicateKeyError(err error) bool {
+	var mysqlErr *mysql.MySQLError
+	if errors.As(err, &mysqlErr) {
+		if mysqlErr.Number == 1062 {
+			return true
+		}
+	}
+	return false
 }
