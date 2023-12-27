b2e1a52caa92b5a088be7945c010d1c123921b2c
NadimRifaii <nadimrifaii3@gmail.com>
2023-12-27 12:44:29 +0200

    implemented the validateUserRequest to validate the request body when login or signup

--- server/controllers/auth.controller.go
+++ server/controllers/auth.controller.go
@@ -1,12 +1,24 @@
 package controllers
 
 import (
+	"errors"
+
+	"github.com/NadimRifaii/campverse/models"
 	"github.com/gofiber/fiber/v2"
 )
 
 func Signup(c *fiber.Ctx) error {
 
 }
+func validateUserRequest(c *fiber.Ctx, user *models.User) error {
+	if err := c.BodyParser(user); err != nil {
+		return errors.New("invalid request body")
+	} else if user.Email == "" || user.Password == "" {
+		return errors.New("missing credentials")
+	}
+	return nil
+}
+
 func loger(c *fiber.Ctx, status int, m fiber.Map) error {
 	return c.Status(status).JSON(m)
 }
--- server/models/user.model.go
+++ server/models/user.model.go
@@ -4,10 +4,10 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Username     string `json:"username" gorm:"not null;default:'first';size:255"`
-	Lastname     string `json:"lastname" gorm:"not null;default:'last';size:255"`
-	Email        string `json:"email" gorm:"not null;size:255;unique"`
-	PasswordHash string `json:"password" gorm:"not null;size:255"`
-	RoleID       uint
-	UserRole     UserRole `gorm:"foreignKey:RoleID"`
+	Username string `json:"username" gorm:"not null;default:'first';size:255"`
+	Lastname string `json:"lastname" gorm:"not null;default:'last';size:255"`
+	Email    string `json:"email" gorm:"not null;size:255;unique"`
+	Password string `json:"password" gorm:"not null;size:255"`
+	RoleID   uint
+	UserRole UserRole `gorm:"foreignKey:RoleID"`
 }
