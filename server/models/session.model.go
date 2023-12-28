24174324879d1aa3035491e2154948720647dc2f
NadimRifaii <nadimrifaii3@gmail.com>
2023-12-28 22:09:50 +0200

    added Session belongs to a Day relation in Session model

--- server/models/session.model.go
+++ server/models/session.model.go
@@ -9,4 +9,6 @@ type Session struct {
 	SessionTitle string `json:"bootcatITLE" gorm:"not null;size:255"`
 	StartTime    string `json:"startTime" gorm:"not null;size:255"`
 	EndTime      string `json:"endTime" gorm:"not null;size:255"`
+	DayId        uint
+	Day          Day
 }
