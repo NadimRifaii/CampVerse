08c2597273f406758ba06e972ba5d92127575609
NadimRifaii <nadimrifaii3@gmail.com>
2023-12-28 18:15:50 +0200

    implemented handleMentorAction helper function in mentor controller

--- server/controllers/mentor.controller.go
+++ server/controllers/mentor.controller.go
@@ -10,41 +10,30 @@ import (
 )
 
 func HttpAddStackToMentor(c *fiber.Ctx) error {
-	user := new(models.User)
-	if user = GetAuthUser(c); user == nil || user.UserRole.RoleName != "mentor" {
-		return Loger(c, fiber.StatusUnauthorized, fiber.Map{"error": "Unauthorized"})
-	}
-	//
-	mentor := new(models.Mentor)
-	db := database.Db
-	if err := mentor.GetMentorByID(db, user.ID); err != nil {
-		return Loger(c, fiber.StatusNotFound, fiber.Map{"error": err.Error()})
-	}
-	mentor.User = *user
-	//
-	stack := new(models.Stack)
-	if err := verifyStackRequest(c, stack); err != nil {
-		return Loger(c, fiber.StatusBadRequest, fiber.Map{"error": err.Error()})
-	}
-	//
-	if err := stack.GetStackByName(db, stack.Name); err != nil {
-		return Loger(c, fiber.StatusBadRequest, fiber.Map{"error": err.Error()})
-	}
-	if err := mentor.AddStackToMentor(db, stack); err != nil {
-		return Loger(c, fiber.StatusBadRequest, fiber.Map{"error": err.Error()})
-	}
-	return Loger(c, fiber.StatusAccepted, fiber.Map{"mentor": mentor})
+	return handleMentorAction(c, "add")
 }
 func HttpRemoveStackFromMentor(c *fiber.Ctx) error {
+	return handleMentorAction(c, "remove")
+}
+func handleMentorAction(c *fiber.Ctx, action string) error {
 	db := database.Db
 	mentor, stack, err := getMentorAndStack(c, db)
 	if err != nil {
 		return Loger(c, fiber.StatusBadRequest, fiber.Map{"error": err.Error()})
 	}
-	if err := mentor.RemoveStackFromMentor(db, stack); err != nil {
-		return Loger(c, fiber.StatusBadRequest, fiber.Map{"error": err.Error()})
+	switch action {
+	case "add":
+		if err := mentor.AddStackToMentor(db, stack); err != nil {
+			return Loger(c, fiber.StatusBadRequest, fiber.Map{"error": err.Error()})
+		}
+	case "remove":
+		if err := mentor.RemoveStackFromMentor(db, stack); err != nil {
+			return Loger(c, fiber.StatusBadRequest, fiber.Map{"error": err.Error()})
+		}
+	default:
+		return Loger(c, fiber.StatusBadRequest, fiber.Map{"error": "Invalid action"})
 	}
-	return Loger(c, fiber.StatusAccepted, fiber.Map{"mentor": mentor})
+	return Loger(c, fiber.StatusAccepted, fiber.Map{"message": "Mentor action successfull"})
 }
 func verifyStackRequest(c *fiber.Ctx, stack *models.Stack) error {
 	if err := c.BodyParser(stack); err != nil {
