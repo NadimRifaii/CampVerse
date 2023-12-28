672071a43ec7bc7fa28146f241507e6061b88762
NadimRifaii <nadimrifaii3@gmail.com>
2023-12-28 11:02:44 +0200

    implemented CreatePredefinedRoles receiver function in user role model

--- server/models/user-role.model.go
+++ server/models/user-role.model.go
@@ -1,6 +1,10 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
 
 type UserRole struct {
 	gorm.Model
@@ -18,4 +22,32 @@ func (ur *UserRole) GetIDByRoleName(db *gorm.DB, roleName string) (uint, error) 
 		return 0, err
 	}
 	return ur.ID, nil
+}
+func (ur *UserRole) CreatePredefinedRoles(db *gorm.DB) {
+	roles := []string{"admin", "user", "mentor"}
+
+	for _, roleName := range roles {
+		var existingRole UserRole
+		result := db.Where("role_name = ?", roleName).First(&existingRole)
+
+		if result.Error != nil && result.Error != gorm.ErrRecordNotFound {
+			fmt.Printf("Error checking existing role: %v\n", result.Error)
+			continue
+		}
+
+		if result.Error == gorm.ErrRecordNotFound {
+			// Role does not exist, add it to the database
+			newRole := UserRole{RoleName: roleName}
+			result := db.Create(&newRole)
+
+			if result.Error != nil {
+				fmt.Printf("Error creating role %s: %v\n", roleName, result.Error)
+				continue
+			}
+
+			fmt.Printf("Role %s added successfully\n", roleName)
+		} else {
+			fmt.Printf("Role %s already exists\n", roleName)
+		}
+	}
 }
