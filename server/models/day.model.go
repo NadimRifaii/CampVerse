226b5125156fda470883975290746b4c783fe188
NadimRifaii <nadimrifaii3@gmail.com>
2023-12-28 22:07:33 +0200

    added the Day model and made relation belongs to schedule

---
+++ server/models/day.model.go
@@ -0,0 +1,10 @@
+package models
+
+import "gorm.io/gorm"
+
+type Day struct {
+	gorm.Model
+	Day        string `json:"day" gorm:"not null;default:'monday';size:255"`
+	ScheduleId uint
+	Schedule   Schedule
+}
