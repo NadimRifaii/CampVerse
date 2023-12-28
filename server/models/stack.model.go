2443fa7f129ddcc61735ef60525debe6ce5ef991
NadimRifaii <nadimrifaii3@gmail.com>
2023-12-28 07:57:39 +0200

    created the GetStacks receiver function in Stack model

--- server/models/stack.model.go
+++ server/models/stack.model.go
@@ -6,3 +6,11 @@ type Stack struct {
 	gorm.Model
 	Name string `json:"name" gorm:"not null;default:'x';size:255;unique"`
 }
+
+func (stack *Stack) GetStacks(db *gorm.DB) ([]Stack, error) {
+	var stacks []Stack
+	if err := db.Find(stack).Error; err != nil {
+		return nil, err
+	}
+	return stacks, nil
+}
