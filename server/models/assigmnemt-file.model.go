98198e512f6944a0633a5a6d5b259f9eb47ae1cb
NadimRifaii <nadimrifaii3@gmail.com>
2023-12-29 23:56:12 +0200

    created AssignmentFile model and made a relation , an assignment has many assignment files

---
+++ server/models/assigmnemt-file.model.go
@@ -0,0 +1,12 @@
+package models
+
+import "gorm.io/gorm"
+
+type AssignmentFile struct {
+	gorm.Model
+	FileName     string `json:"fileName" gorm:"not null;size:255;unique"`
+	FileType     string `json:"fileType" gorm:"default:'pdf';size:255"`
+	FileUrl      string `json:"fileUrl" gorm:"not null;size:255;unique"`
+	AssignmentId uint
+	Assignment   Assignment
+}
--- server/models/assignment.model.go
+++ server/models/assignment.model.go
@@ -4,16 +4,14 @@ import "gorm.io/gorm"
 
 type Assignment struct {
 	gorm.Model
-	FileName    string `json:"fileName" gorm:"not null;size:255;unique"`
-	FileType    string `json:"fileType" gorm:"default:'pdf';size:255"`
-	FileUrl     string `json:"fileUrl" gorm:"not null;size:255;unique"`
-	Description string `json:"fileDescription" gorm:"not null;size:255"`
-	MentorId    uint
-	Mentor      Mentor
-	StackId     uint
-	Stack       Stack
-	BootcampId  uint
-	Bootcamp    Bootcamp
+	Description     string `json:"fileDescription" gorm:"not null;size:255"`
+	MentorId        uint
+	Mentor          Mentor
+	StackId         uint
+	Stack           Stack
+	BootcampId      uint
+	Bootcamp        Bootcamp
+	AssignmentFiles []*AssignmentFile
 }
 
 func (a *Assignment) GetAllAssignments(db *gorm.DB) ([]Assignment, error) {
