3cdecd9ae452bf31e5b768eef7b54206a7a1162b
NadimRifaii <nadimrifaii3@gmail.com>
2023-12-30 00:22:59 +0200

    implemented GetAllSubmissions receiver function in StudentSubmission model

--- server/models/student-submission.model.go
+++ server/models/student-submission.model.go
@@ -13,3 +13,15 @@ type StudentSubmission struct {
 	BootcampId  uint
 	Bootcamp    Bootcamp
 }
+
+func (studentSubmission *StudentSubmission) GetAllSubmissions(db *gorm.DB) ([]StudentSubmission, error) {
+	var submissions []StudentSubmission
+
+	if err := db.Preload("Bootcamp").Preload("Student").Preload("Stack").Find(&submissions).Error; err != nil {
+		return nil, err
+	}
+	/*
+		Preload method to eager load the associated Bootcamp, Mentor, and Stack for each assignment
+	*/
+	return submissions, nil
+}
