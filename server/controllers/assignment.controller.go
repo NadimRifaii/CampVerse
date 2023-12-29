e7e187080a63c056eeb01e10f6f40b1a3f962d7d
NadimRifaii <nadimrifaii3@gmail.com>
2023-12-29 18:49:25 +0200

    created the assignment route and added a route that maps to HttpCreateAssignment controller

--- server/controllers/assignment.controller.go
+++ server/controllers/assignment.controller.go
@@ -8,7 +8,11 @@ import (
 )
 
 func HttpCreateAssignment(c *fiber.Ctx) error {
-	return nil
+	assignment := new(models.Assignment)
+	if err := validateAssignmentRequest(c, assignment); err != nil {
+		return Loger(c, fiber.StatusBadRequest, fiber.Map{"error": err.Error()})
+	}
+	return Loger(c, fiber.StatusAccepted, fiber.Map{"assignment": assignment})
 }
 func validateAssignmentRequest(c *fiber.Ctx, assignment *models.Assignment) error {
 	if err := c.BodyParser(assignment); err != nil {
