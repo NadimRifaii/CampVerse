51fdb820c9ec7da06d8987cd59320fd79345d7ff
NadimRifaii <nadimrifaii3@gmail.com>
2023-12-30 03:34:52 +0200

    testing HttpSubmitAssignment controller in student controller

--- server/controllers/student.controller.go
+++ server/controllers/student.controller.go
@@ -1,7 +1,15 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	// "github.com/NadimRifaii/campverse/database"
+	"github.com/NadimRifaii/campverse/models"
+	"github.com/gofiber/fiber/v2"
+)
 
-func HttpSubmitAssignment(c *fiber.Ctx) {
-
+func HttpSubmitAssignment(c *fiber.Ctx) error {
+	user := new(models.User)
+	// db := database.Db
+	if user = GetAuthUser(c); user == nil {
+		return Loger(c, fiber.StatusUnauthorized, fiber.Map{"error": "Unauthorized"})
+	}
 }
