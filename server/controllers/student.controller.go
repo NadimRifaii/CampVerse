bcbf6e2209bc83c202b7f682b0b6eab5293df198
NadimRifaii <nadimrifaii3@gmail.com>
2023-12-30 03:38:02 +0200

    implemented GetStudent function in student controller

--- server/controllers/student.controller.go
+++ server/controllers/student.controller.go
@@ -2,8 +2,11 @@ package controllers
 
 import (
 	// "github.com/NadimRifaii/campverse/database"
+	"errors"
+
 	"github.com/NadimRifaii/campverse/models"
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
 )
 
 func HttpSubmitAssignment(c *fiber.Ctx) error {
@@ -12,4 +15,19 @@ func HttpSubmitAssignment(c *fiber.Ctx) error {
 	if user = GetAuthUser(c); user == nil {
 		return Loger(c, fiber.StatusUnauthorized, fiber.Map{"error": "Unauthorized"})
 	}
+	return Loger(c, fiber.StatusAccepted, fiber.Map{})
+}
+func GetStudent(c *fiber.Ctx, db *gorm.DB) (*models.Student, error) {
+	user := new(models.User)
+	if user = GetAuthUser(c); user == nil || user.UserRole.RoleName != "student" {
+		return nil, errors.New("Unauthorized")
+	}
+
+	mentor := new(models.Student)
+	if err := student.GetStudentByID(db, user.ID); err != nil {
+		return nil, errors.New("Unauthorized")
+	}
+	mentor.User = *user
+
+	return student, nil
 }
