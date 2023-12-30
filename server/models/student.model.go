d3b98da834bea420625d1b33b4426c9c5a53f785
NadimRifaii <nadimrifaii3@gmail.com>
2023-12-30 03:39:38 +0200

    implemented the GetStudentById receiver function in student model

--- server/models/student.model.go
+++ server/models/student.model.go
@@ -1,6 +1,10 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"errors"
+
+	"gorm.io/gorm"
+)
 
 type Student struct {
 	gorm.Model
@@ -8,4 +12,11 @@ type Student struct {
 	User              User
 	SubmissionFile    []*SubmissionFile
 	StudentSubmission []*StudentSubmission
+}
+
+func (student *Student) GetStudentByID(db *gorm.DB, id uint) error {
+	if db.Find(student, "user_id=?", id); student.ID == 0 {
+		return errors.New("student was not found")
+	}
+	return nil
 }
