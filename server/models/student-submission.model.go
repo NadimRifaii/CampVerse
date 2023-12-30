743a0f230faefc98502eaff1631bb700dd8a32b0
NadimRifaii <nadimrifaii3@gmail.com>
2023-12-30 05:17:23 +0200

    fixed return error in CreateSubmission receiver function

--- server/models/student-submission.model.go
+++ server/models/student-submission.model.go
@@ -17,10 +17,10 @@ type StudentSubmission struct {
 	SubmissionFile []*SubmissionFile `json:"files"`
 }
 
-func (submission *StudentSubmission) CreateOrUpdateSubmission(db *gorm.DB) error {
+func (submission *StudentSubmission) CreateSubmission(db *gorm.DB) error {
 	var existingSubmission StudentSubmission
 	if db.First(&existingSubmission, "stack_id = ? AND student_id = ? AND assignment_id = ?", submission.StackId, submission.StudentId, submission.AssignmentId); existingSubmission.ID != 0 {
-		return errors.New("Submission already exists for the given Stack, Student, and Assignment")
+		return errors.New("submission already exists for the given Stack, Student, and Assignment")
 	}
 	if err := db.Create(submission).Error; err != nil {
 		return err
