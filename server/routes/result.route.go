7551a47a4437803e7a66600f2728fdf4b0398656
NadimRifaii <nadimrifaii3@gmail.com>
2023-12-30 14:55:12 +0200

    created result route

---
+++ server/routes/result.route.go
@@ -0,0 +1,10 @@
+package routes
+
+import (
+	"github.com/NadimRifaii/campverse/controllers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func ResultRoutes(app fiber.Router) {
+	app.Post("/", controllers.HttpCreateResults)
+}
