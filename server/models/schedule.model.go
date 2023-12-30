a55eeca9b247548360f799e84afb4f3971b46ba9
NadimRifaii <nadimrifaii3@gmail.com>
2023-12-30 11:59:35 +0200

    moved the day model and session model into the schedule model since they are related

--- server/models/schedule.model.go
+++ server/models/schedule.model.go
@@ -11,6 +11,27 @@ type Schedule struct {
 	Week string `json:"week" gorm:"not null;size:255;unique"`
 	Days []*Day `gorm:"foreignKey:ScheduleId"`
 }
+type Day struct {
+	gorm.Model
+	Day        string `json:"day" gorm:"not null;default:'monday';size:255"`
+	ScheduleId uint
+	Sessions   []*Session `gorm:"foreignKey:DayId"`
+}
+
+type Session struct {
+	gorm.Model
+	SessionTitle string `json:"title" gorm:"not null;size:255"`
+	StartTime    string `json:"startTime" gorm:"not null;size:255"`
+	EndTime      string `json:"endTime" gorm:"not null;size:255"`
+	DayId        uint
+}
+
+func (day *Day) GetDaySessions(db *gorm.DB) error {
+	if err := db.Model(day).Association("Sessions").Find(&day.Sessions); err != nil {
+		return err
+	}
+	return nil
+}
 
 func (schedule *Schedule) GetScheduleByWeek(db *gorm.DB) error {
 	if db.Find(schedule, "week = ?", schedule.Week); schedule.ID == 0 {
