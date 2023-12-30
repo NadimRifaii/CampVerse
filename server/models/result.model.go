f8476d5462d14d1ae28a4822144155d6b953df99
NadimRifaii <nadimrifaii3@gmail.com>
2023-12-30 14:49:48 +0200

    added result and grade model , student result have many grades

--- server/models/result.model.go
+++ server/models/result.model.go
@@ -1,1 +1,17 @@
 package models
+
+import "gorm.io/gorm"
+
+type Result struct {
+	gorm.Model
+	StudentId  uint
+	BootcampId uint
+	Week       string `json:"week" gorm:"unique"`
+}
+type Grade struct {
+	gorm.Model
+	ResultId uint
+	StackId  uint
+	Grade    int    `json:"grade"`
+	Badge    string `json:"badge"`
+}
