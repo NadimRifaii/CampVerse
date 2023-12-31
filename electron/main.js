3f763d3e16bfba1f5e08a86466d5f5659e3d864c
NadimRifaii <nadimrifaii3@gmail.com>
2024-01-01 00:25:19 +0200

    created main.js file and added start script to launch electron project

---
+++ electron/main.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const url = require('url')
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    title: "Electron",
+    width: 1000,
+    height: 600
+  })
+  const startUrl = url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file'
+  })
+  mainWindow.loadURL(startUrl)
+}
+app.whenReady().then(createMainWindow)
