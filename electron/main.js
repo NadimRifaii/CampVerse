d4837751315d209084a79516398bcb50ef7dbb88
NadimRifaii <nadimrifaii3@gmail.com>
2024-01-01 08:01:12 +0200

    build the react application and launched the index.html inside of the build folder in electron app

--- electron/main.js
+++ electron/main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
+const { start } = require('repl')
 const url = require('url')
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
@@ -7,10 +8,13 @@ function createMainWindow() {
     width: 1000,
     height: 600
   })
+  mainWindow.webContents.openDevTools()
   const startUrl = url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file'
+    pathname: path.join(__dirname, './admin-dashboard/build/index.html'),
+    protocol: 'file',
+
   })
+  // mainWindow.loadURL(`http://localhost:3000`)
   mainWindow.loadURL(startUrl)
 }
 app.whenReady().then(createMainWindow)
