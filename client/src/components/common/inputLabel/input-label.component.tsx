4136fd885376bfcb94e799b20db0028f5317969d
NadimRifaii <nadimrifaii3@gmail.com>
2024-01-02 09:46:06 +0200

    added form validation to signup form

--- client/src/components/common/inputLabel/input-label.component.tsx
+++ client/src/components/common/inputLabel/input-label.component.tsx
@@ -1,13 +1,17 @@
 import './input-label.styles.css'
 type InputProps = {
   type: string,
-  label: string
+  label: string,
+  name: string,
+  value: string,
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
-export const InputLabel = ({ type, label }: InputProps) => {
+export const InputLabel = ({ type, label, name, value, handleChange }: InputProps) => {
+  const minLength = type === 'password' ? 6 : undefined;
   return (
     <div className="input-label">
-      <input type={type} />
-      <label htmlFor="">{label}</label>
+      <input type={type} name={name} minLength={minLength} value={value} required onChange={handleChange} />
+      <label className={`${value ? 'active' : ''}`} htmlFor="">{label}</label>
     </div>
   )
 }
--- client/src/components/signupForm/signup.component.jsx
+++ client/src/components/signupForm/signup.component.jsx
@@ -1,34 +1,54 @@
-import { Button } from "../common/button/button.component"
-import { InputLabel } from "../common/inputLabel/input-label.component"
-import { ReactComponent as MyIcon } from '../../Google.svg'
-import { useState } from 'react'
-import './signup.styles.css'
+import { Button } from "../common/button/button.component";
+import { InputLabel } from "../common/inputLabel/input-label.component";
+import { ReactComponent as MyIcon } from '../../Google.svg';
+import { useState } from 'react';
+import './signup.styles.css';
+
 const defaultFormFields = {
   firstname: "",
   lastname: "",
   email: "",
   password: "",
-  role: ""
-}
+  role: "mentor",
+};
 
 export const Signup = () => {
-  const [formFields, setFormFields] = useState(defaultFormFields)
-  const { firstname, lastname, email, password, role } = formFields
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { firstname, lastname, email, password, role } = formFields;
+
+  const changeHandler = (event) => {
+    setFormFields({ ...formFields, [event.target.name]: event.target.value });
+
+  };
+  console.log(formFields)
   return (
     <div className="signup">
-      <form action="" onSubmit={(e) => {
-        e.preventDefault()
-      }} >
+      <form
+        action=""
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
         <h1>Create account</h1>
-        <div className="google-icon" >
+        <div className="google-icon">
           <MyIcon />
         </div>
-        <InputLabel type="text" label="First name" name='firstname' />
-        <InputLabel type="text" label="Last name" name='lastname' />
-        <InputLabel type="email" label="Email" name='email' />
-        <InputLabel type="password" label="Password" name='password' />
+        <InputLabel type="text" label="First name" value={firstname} name='firstname' handleChange={changeHandler} />
+        <InputLabel type="text" label="Last name" value={lastname} name='lastname' handleChange={changeHandler} />
+        <InputLabel type="email" label="Email" value={email} name='email' handleChange={changeHandler} />
+        <InputLabel type="password" label="Password" value={password} name='password' handleChange={changeHandler} />
+        <div className="radio-holder">
+          <div className="holder">
+            <input type="radio" id="student" name='role' value='student' onChange={changeHandler} />
+            <label htmlFor="student">Student</label>
+          </div>
+          <div className="holder">
+            <input type="radio" id="mentor" name='role' value='mentor' onChange={changeHandler} checked={role === 'mentor'} />
+            <label htmlFor="mentor">Mentor</label>
+          </div>
+        </div>
         <Button text="Signup" />
       </form>
     </div>
-  )
-}
+  );
+};
