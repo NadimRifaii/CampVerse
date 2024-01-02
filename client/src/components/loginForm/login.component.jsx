5bea2d26f38709dcd09b5af495f0fb7a5ab2f53c
NadimRifaii <nadimrifaii3@gmail.com>
2024-01-02 08:41:21 +0200

    fixing css for auth page

--- client/src/components/loginForm/login.component.jsx
+++ client/src/components/loginForm/login.component.jsx
@@ -1,11 +1,12 @@
 import { Button } from "../common/button/button.component"
 import { InputLabel } from "../common/inputLabel/input-label.component"
 import { ReactComponent as MyIcon } from '../../Google.svg'
-import './login.styles.css'
 export const Login = () => {
   return (
     <div className="login">
-      <form action="">
+      <form action="" onSubmit={(e) => {
+        e.preventDefault()
+      }} >
         <h1>login</h1>
         <div className="google-icon" >
           <MyIcon />
--- client/src/components/signupForm/signup.component.jsx
+++ client/src/components/signupForm/signup.component.jsx
@@ -1,19 +1,32 @@
 import { Button } from "../common/button/button.component"
 import { InputLabel } from "../common/inputLabel/input-label.component"
 import { ReactComponent as MyIcon } from '../../Google.svg'
+import { useState } from 'react'
+import './signup.styles.css'
+const defaultFormFields = {
+  firstname: "",
+  lastname: "",
+  email: "",
+  password: "",
+  role: ""
+}
 
 export const Signup = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields)
+  const { firstname, lastname, email, password, role } = formFields
   return (
     <div className="signup">
-      <form action="">
+      <form action="" onSubmit={(e) => {
+        e.preventDefault()
+      }} >
         <h1>Create account</h1>
         <div className="google-icon" >
           <MyIcon />
         </div>
-        <InputLabel type="text" label="First name" />
-        <InputLabel type="text" label="Last name" />
-        <InputLabel type="email" label="Email" />
-        <InputLabel type="password" label="Password" />
+        <InputLabel type="text" label="First name" name='firstname' />
+        <InputLabel type="text" label="Last name" name='lastname' />
+        <InputLabel type="email" label="Email" name='email' />
+        <InputLabel type="password" label="Password" name='password' />
         <Button text="Signup" />
       </form>
     </div>
