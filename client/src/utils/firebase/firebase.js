9065f32290c8ddf6fe7e0e61c860047f40b8c027
NadimRifaii <nadimrifaii3@gmail.com>
2024-01-02 20:24:05 +0200

    implemented signInWithGooglePopup in firebase.js

--- client/src/utils/firebase/firebase.js
+++ client/src/utils/firebase/firebase.js
@@ -3,6 +3,8 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDxwdgyPl9zwMQ8EAlJvWlYq5jZCxRYFNw",
@@ -15,3 +17,11 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider()
+provider.setCustomParameters({
+  prompt: "select_account"
+})
+
+export const auth = getAuth()
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
