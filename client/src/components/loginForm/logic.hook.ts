1fadb2043fa375bf6e432e9b05806790ef93a6d3
NadimRifaii <nadimrifaii3@gmail.com>
2024-01-03 19:53:29 +0200

    implemented logic hook of login form

--- client/src/components/loginForm/logic.hook.ts
+++ client/src/components/loginForm/logic.hook.ts
@@ -2,14 +2,48 @@ import { local } from "../../core/helpers/localStorage";
 import { useState } from "react";
 import { useDispatch } from 'react-redux'
 import { LoginCredentials } from "../../core/types/loginCredentials";
+import { setUser } from "../../core/datasource/localDataSource/user/userSlice";
+import toast from "react-hot-toast";
+import { authDataSource } from "../../core/datasource/remoteDataSource/auth";
+import { signInWithGooglePopup } from "../../utils/firebase/firebase";
+const defaultCredentials: LoginCredentials = {
+  email: "",
+  password: "",
+};
 export const useLogic = () => {
-  const [credentials, setCredentials] = useState<LoginCredentials>({
-    email: "",
-    password: ''
-  })
+  const [credentials, setCredentials] = useState<LoginCredentials>(defaultCredentials)
+  const [googleSignInComplete, setGoogleSignInComplete] = useState(false);
   const dispatch = useDispatch()
   const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     setCredentials({ ...credentials, [event.target.name]: event.target.value });
   };
-  return { changeHandler }
+  function handleLogin(data: any) {
+    dispatch(setUser(data.user))
+    setGoogleSignInComplete(false);
+    setCredentials({ ...defaultCredentials })
+  }
+  const loginClick = async () => {
+    const loadingToastId = toast.loading('Logging in...');
+    try {
+      const data = await authDataSource.login(credentials)
+      local("token", data.token)
+      handleLogin(data)
+      toast.success('Login successful!', { id: loadingToastId });
+    } catch (error) {
+      setCredentials({ ...defaultCredentials })
+      toast.error(`${error}`, { id: loadingToastId });
+    }
+  }
+  const signInWithGoogle = async () => {
+    try {
+      const response = await signInWithGooglePopup()
+      const userAuth = response.user
+      if (userAuth.email !== null && userAuth.uid !== null)
+        setCredentials({ ['email']: userAuth.email, ['password']: userAuth.uid })
+      setGoogleSignInComplete(true);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  return { changeHandler, signInWithGoogle, loginClick, googleSignInComplete, credentials }
 }
--- client/src/components/loginForm/login.component.jsx
+++ client/src/components/loginForm/login.component.jsx
@@ -3,69 +3,24 @@ import { InputLabel } from "../common/inputLabel/input-label.component"
 import { ReactComponent as MyIcon } from '../../assets/continue-with-google.svg'
 import { useContext, useState, useEffect } from 'react'
 import { ActiveFormContext } from "../../utils/contexts/active-form.context"
-import { signInWithGooglePopup } from "../../utils/firebase/firebase"
-import { toast } from 'react-hot-toast';
-import { useDispatch } from "react-redux";
-import { setUser } from "../../core/datasource/localDataSource/user/userSlice"
-import { sendRequest } from "../../core/helpers/request"
-import { local } from "../../core/helpers/localStorage"
-import { authDataSource } from "../../core/datasource/remoteDataSource/auth"
-const defaultFormFields = {
-  email: "",
-  password: "",
-};
+import { useLogic } from "./logic.hook"
 export const Login = () => {
-  const [formFields, setFormFields] = useState(defaultFormFields);
-  const [googleSignInComplete, setGoogleSignInComplete] = useState(false);
-  const dispatch = useDispatch()
-  const { email, password } = formFields;
-
+  const { changeHandler, googleSignInComplete, loginClick, signInWithGoogle, credentials } = useLogic()
   useEffect(() => {
     if (googleSignInComplete) {
       loginClick();
     }
   }, [googleSignInComplete]);
-  const changeHandler = (event) => {
-    setFormFields({ ...formFields, [event.target.name]: event.target.value });
-  };
   const activeFormContext = useContext(ActiveFormContext)
   if (!activeFormContext) {
     return <h1>activeContext not found</h1>
   }
   const { setActive } = activeFormContext
-  function handleLogin(data) {
-    dispatch(setUser(data.user))
-    setGoogleSignInComplete(false);
-    setFormFields({ ...defaultFormFields })
-  }
-  const loginClick = async () => {
-    const loadingToastId = toast.loading('Logging in...');
-    try {
-      const data = await authDataSource.login(formFields)
-      local("token", data.token)
-      handleLogin(data)
-      toast.success('Login successful!', { id: loadingToastId });
-    } catch (error) {
-      setFormFields({ ...defaultFormFields })
-      toast.error(`${error}`, { id: loadingToastId });
-    }
-  }
-  const signInWithGoogle = async () => {
-    try {
-      const response = await signInWithGooglePopup()
-      const userAuth = response.user
-      setFormFields({ ['email']: userAuth.email, ['password']: userAuth.uid })
-      setGoogleSignInComplete(true);
-    } catch (error) {
-      console.log(error)
-    }
-  }
   return (
     <div className="login">
       <form action="" onSubmit={(e) => {
         e.preventDefault()
         loginClick()
-        setFormFields({ ...defaultFormFields })
       }} >
         <h1>login</h1>
         <div className="google-icon" onClick={signInWithGoogle} >
@@ -74,8 +29,8 @@ export const Login = () => {
         <p>
           Or continue with your account
         </p>
-        <InputLabel type="email" label="email" name="email" value={email} handleChange={changeHandler} />
-        <InputLabel type="password" label="password" name="password" value={password} handleChange={changeHandler} />
+        <InputLabel type="email" label="email" name="email" value={credentials.email} handleChange={changeHandler} />
+        <InputLabel type="password" label="password" name="password" value={credentials.password} handleChange={changeHandler} />
         <Button className="submit" text="Login" />
       </form>
       <div className="switch-form">
