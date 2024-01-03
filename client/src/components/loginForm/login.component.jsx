acb79bb3c5d1e70089f2830e00a9aff1ff1999d4
NadimRifaii <nadimrifaii3@gmail.com>
2024-01-03 09:10:34 +0200

    added the google logo to the assets folder

--- client/src/components/loginForm/login.component.jsx
+++ client/src/components/loginForm/login.component.jsx
@@ -1,6 +1,6 @@
 import { Button } from "../common/button/button.component"
 import { InputLabel } from "../common/inputLabel/input-label.component"
-import { ReactComponent as MyIcon } from '../../continue-with-google.svg'
+import { ReactComponent as MyIcon } from '../../assets/continue-with-google.svg'
 import { useContext, useState, useEffect } from 'react'
 import { ActiveFormContext } from "../../utils/contexts/active-form.context"
 import { signInWithGooglePopup } from "../../utils/firebase/firebase"
--- client/src/components/signupForm/signup.component.jsx
+++ client/src/components/signupForm/signup.component.jsx
@@ -1,6 +1,6 @@
 import { Button } from "../common/button/button.component";
 import { InputLabel } from "../common/inputLabel/input-label.component";
-import { ReactComponent as MyIcon } from '../../continue-with-google.svg';
+import { ReactComponent as MyIcon } from '../../assets/continue-with-google.svg';
 import { useState, useContext, useEffect } from 'react';
 import './signup.styles.css';
 import { ActiveFormContext } from "../../utils/contexts/active-form.context";
