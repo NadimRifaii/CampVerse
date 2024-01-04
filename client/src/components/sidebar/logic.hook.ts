1d48c501ec444c10a00a2ab3c8526907960ece17
NadimRifaii <nadimrifaii3@gmail.com>
2024-01-04 20:18:17 +0200

    Frontend: finished toggle action of sidebar

--- client/src/components/sidebar/logic.hook.ts
+++ client/src/components/sidebar/logic.hook.ts
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 const useLogic = () => {
-  const [sidebarActive, setSidebarActive] = useState<boolean>(false)
+  const [sidebarActive, setSidebarActive] = useState<boolean>(true)
   return { sidebarActive, setSidebarActive }
 }
 export default useLogic
--- client/src/components/sidebar/sidebar.component.jsx
+++ client/src/components/sidebar/sidebar.component.jsx
@@ -1,10 +1,13 @@
 import Logo from "../../assets/logo.component"
 import SidebarToggler from "../../assets/sidebar-toggler-icon"
 import SmallLogo from "../../assets/small-logo.component"
+import ResultsIcon from "../../assets/users-icon.component"
+import useLogic from "./logic.hook"
 import './sidebar.styles.css'
 export const SideBar = () => {
+  const { sidebarActive, setSidebarActive } = useLogic()
   return (
-    <div className="sidebar">
+    <div className={`sidebar ${sidebarActive ? 'hidden' : ""} `}>
       <div className="top">
         <div className="logo-title">
           <div className="logo">
@@ -14,8 +17,18 @@ export const SideBar = () => {
             CampVerse
           </div>
         </div>
-        <div className="toggler">
+        <div className="toggler" onClick={() => setSidebarActive(!sidebarActive)}>
           <SidebarToggler />
+        </div>
+      </div>
+      <div className="sidebar-items-container">
+        <div className="sidebar-item">
+          <div className="icon">
+            <ResultsIcon />
+          </div>
+          <div className="text">
+            Results
+          </div>
         </div>
       </div>
     </div>
