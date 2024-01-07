46d3897b75c4b6c7b506a98658bfd81df8a3aa91
NadimRifaii <nadimrifaii3@gmail.com>
2024-01-07 08:07:12 +0200

    Frontend: added EditProfile component

---
+++ client/src/components/editProfile/edit.component.tsx
@@ -0,0 +1,10 @@
+import { profile } from "console"
+
+const EditProfile = () => {
+  return (
+    <div className="edit-container">
+      <h1>Edit Profile</h1>
+    </div>
+  )
+}
+export default EditProfile
