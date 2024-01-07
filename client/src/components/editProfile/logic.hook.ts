cfde58895895de7d66342a3f85fac2ee170fcf7d
NadimRifaii <nadimrifaii3@gmail.com>
2024-01-07 10:44:53 +0200

    Frontend: added logic hook for dashboard component

--- client/src/components/editProfile/logic.hook.ts
+++
@@ -1,8 +0,0 @@
-import { extractUserSlice } from "../../core/datasource/localDataSource/user/userSlice"
-import { useDispatch, useSelector } from "react-redux"
-import { User } from "../../core/types/user"
-const useLogic = () => {
-  const user = useSelector(extractUserSlice)
-
-}
-export default useLogic
---
+++ client/src/pages/dashboard/logic.hook.ts
@@ -0,0 +1,31 @@
+import { extractUserSlice } from "../../core/datasource/localDataSource/user/userSlice"
+import { useDispatch, useSelector } from "react-redux"
+import { userDataSource } from "../../core/datasource/remoteDataSource/user"
+import { authDataSource } from '../../core/datasource/remoteDataSource/auth'
+import { setUser } from '../../core/datasource/localDataSource/user/userSlice'
+import { local } from '../../core/helpers/localStorage'
+import { useNavigate } from "react-router-dom"
+const useLogic = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const user = useSelector(extractUserSlice)
+  async function refresh() {
+    try {
+      const data = await authDataSource.refresh({})
+      dispatch(setUser(data.user))
+      local("token", data.token)
+    } catch (error) {
+      return navigate('/')
+    }
+  }
+  async function getUserInfo() {
+    try {
+      const data = await userDataSource.getUser({})
+      console.log(data)
+    } catch (error: any) {
+      console.log(error)
+    }
+  }
+  return { getUserInfo, refresh }
+}
+export default useLogic
