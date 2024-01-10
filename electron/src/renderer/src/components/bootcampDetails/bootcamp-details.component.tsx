60274548803c779c1028ec6e0aabee4e49083271
NadimRifaii <nadimrifaii3@gmail.com>
2024-01-11 00:37:37 +0200

    Electron: added the currentActiveComponent in the bootcamp details component

--- electron/src/renderer/src/components/bootcampDetails/bootcamp-details.component.tsx
+++ electron/src/renderer/src/components/bootcampDetails/bootcamp-details.component.tsx
@@ -1,13 +1,23 @@
 import { useSelector } from "react-redux"
 import { extractcurrentBootcampSlice } from "@renderer/core/datasource/localDataSource/currentBootcamp/currentBootcampSlice"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { Button } from "../common/button/button.component"
+import './bootcamp-details.styles.css'
 const BootcampDetails = () => {
   const currentBootcamp = useSelector(extractcurrentBootcampSlice)
+  const [currentActiveComponent, setCurrentActiveComponent] = useState<string>('users')
   useEffect(() => {
     console.log(currentBootcamp)
   }, [currentBootcamp])
   return (
-    <h1>Bootcamp detail</h1>
+    <div className="bootcamp-details-container">
+      <div className="toggler-header">
+        <Button text="Users" handleClick={() => setCurrentActiveComponent('users')} className={`${currentActiveComponent == "users" ? 'active' : ""}`} />
+        <Button text="Students" handleClick={() => setCurrentActiveComponent('students')} className={`${currentActiveComponent == "students" ? 'active' : ""}`} />
+        <Button text="Curriculum" handleClick={() => setCurrentActiveComponent('curriculum')} className={`${currentActiveComponent == "curriculum" ? 'active' : ""}`} />
+      </div>
+
+    </div>
   )
 }
 export default BootcampDetails
