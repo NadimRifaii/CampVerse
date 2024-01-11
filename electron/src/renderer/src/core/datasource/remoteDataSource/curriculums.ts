05e6347bb19c05596c282d3d0bfbf4032edd8d61
NadimRifaii <nadimrifaii3@gmail.com>
2024-01-11 23:55:55 +0200

    Electron: added the curriculum remote data source and implemented the getCurriculums function

---
+++ electron/src/renderer/src/core/datasource/remoteDataSource/curriculums.ts
@@ -0,0 +1,16 @@
+import { sendRequest } from "@renderer/core/helpers/request"
+
+export const curriculumsDataSource = {
+  getCurriculums: async (data: {}) => {
+    try {
+      const response = await sendRequest({
+        body: data,
+        route: "/curriculum",
+        method: "POST"
+      })
+      return response
+    } catch (error: any) {
+      throw new Error(error)
+    }
+  },
+}
