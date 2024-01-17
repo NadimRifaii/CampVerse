7017b8c9b86a95a3cc86709fb83d46ada55ea025
NadimRifaii <nadimrifaii3@gmail.com>
2024-01-18 00:43:06 +0200

    Frontend: tested the assignment card component

--- client/src/components/submissionCard/submission-card.component.tsx
+++ client/src/components/submissionCard/submission-card.component.tsx
@@ -1,8 +1,14 @@
 import { useEffect } from "react"
 import { submissionType } from "../../core/datasource/localDataSource/submissions/submissionsSlice"
 import './submission-card.styles.css'
+import AssignmentsIcon from "../../assets/assignments-icon.component"
 type SubmissionCardType = {
-  submission: submissionType
+  submission: {
+    dueDate: string,
+    submitedAt: string,
+    assignmentTitle: string,
+    ID: number
+  }
 }
 
 const SubmissionCard = ({ submission }: SubmissionCardType) => {
@@ -11,7 +17,15 @@ const SubmissionCard = ({ submission }: SubmissionCardType) => {
   }, [])
   return (
     <div className="submission-card">
-
+      <div className="header">
+        <h2>Assignment</h2>
+      </div>
+      <div className="icon">
+        <AssignmentsIcon />
+      </div>
+      <div className="title">
+        {submission.assignmentTitle}
+      </div>
     </div>
   )
 }
