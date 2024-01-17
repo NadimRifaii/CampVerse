3a7f557cab246601ea288dd8bc0b00e8f5ac2ee9
NadimRifaii <nadimrifaii3@gmail.com>
2024-01-17 15:21:57 +0200

    Frontend: added the AssignmentsList component with it's props type

--- client/src/components/assignments/assignment.component.tsx
+++ client/src/components/assignments/assignment.component.tsx
@@ -3,9 +3,10 @@ import CreateAssignment from "../createAssignment/create-assignment.controller"
 import { useState } from "react"
 import './assignments.styles.css'
 import useLogic from "./logic.hook"
+import AssignmentsList from "../assignmentsList/assignments-list.component"
 const Assignment = () => {
   const [currentActiveComponent, setCurrentActiveComponent] = useState<string>("create")
-  const { assignments ,oldAssignments,upcomingAssignments} = useLogic()
+  const { assignments, oldAssignments, upcomingAssignments } = useLogic()
   return (
     <div className="assignments-section">
       <div className="toggler-header">
@@ -13,7 +14,10 @@ const Assignment = () => {
         <Button text="In progress" handleClick={() => setCurrentActiveComponent('progress')} className={`${currentActiveComponent == "progress" ? 'active' : ""}`} />
         <Button text="Create" handleClick={() => setCurrentActiveComponent('create')} className={`${currentActiveComponent == "create" ? 'active' : ""}`} />
       </div>
-      <CreateAssignment />
+      {
+        currentActiveComponent == "create" ? <CreateAssignment /> : <AssignmentsList assignments={currentActiveComponent == 'old' ? oldAssignments : upcomingAssignments} />
+      }
+
     </div>
   )
 }
---
+++ client/src/components/assignmentsList/assignments-list.component.tsx
@@ -0,0 +1,11 @@
+import { Assignment } from "../../core/datasource/localDataSource/assignments/assignmentsSlice"
+
+type AssignmentsListProps = {
+  assignments: Assignment[]
+}
+const AssignmentsList = ({ assignments }: AssignmentsListProps) => {
+  return (
+    <h1></h1>
+  )
+}
+export default AssignmentsList
