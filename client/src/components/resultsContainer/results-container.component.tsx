53529b153a799e5294fad06604be7a8d31056c17
NadimRifaii <nadimrifaii3@gmail.com>
2024-01-22 13:10:07 +0200

    Frontend: added the react-select package

--- client/src/components/resultsContainer/results-container.component.tsx
+++ client/src/components/resultsContainer/results-container.component.tsx
@@ -1,45 +1,50 @@
 import React, { useEffect, useState } from "react";
 import useLogic from "./logic.hook";
 import { CurriculumType } from "../createAssignment/logic.hook";
-import { Grade, Result } from "../../core/datasource/localDataSource/results/resultsSlice";
-import './results-container.styles.css'
 import { Stack } from "../../core/types/stack";
 import ResultsTable from "../resultsTable/results-table.component";
+import Select from 'react-select'
+import './results-container.styles.css';
 
 const ResultsContainer = () => {
   const { results, curriculums, currentBootcamp } = useLogic();
-  const [stacks, setStacks] = useState<string[]>([])
+  const [stacks, setStacks] = useState<string[]>([]);
+
   useEffect(() => {
-    let arr: string[] = []
+    let arr: string[] = [];
     curriculums.map((curriculum: CurriculumType, index: number) => {
       curriculum.stacks.map((stack: Stack, stackIndex: number) => {
-        arr.push(stack.name)
-      })
-    })
-    setStacks(arr)
-  }, [curriculums])
+        arr.push(stack.name);
+      });
+    });
+    setStacks(arr);
+  }, [curriculums]);
+
   useEffect(() => {
-    console.log(results)
-  }, [results])
+    console.log(results);
+  }, [results]);
+
   useEffect(() => {
-    console.log(currentBootcamp)
-  }, [currentBootcamp])
+    console.log(currentBootcamp);
+  }, [currentBootcamp]);
+
   return (
     <div className="results-container">
       <div className="select-box">
-        <select name="week" id="">
-          {
-            currentBootcamp?.weeks.map((week, index: number) => {
-              return (
-                <option value={week.ID} key={index}>Week {week.ID}</option>
-              )
-            })
-          }
-        </select>
+        <Select
+          options={currentBootcamp?.weeks.map((week, index: number) => ({
+            value: week.ID,
+            label: `Week ${week.ID}`,
+          }))}
+          defaultValue={{
+            value: currentBootcamp?.weeks[0].ID,
+            label: `Week ${currentBootcamp?.weeks[0].ID}`,
+          }}
+        />
       </div>
       <ResultsTable results={results} stacks={stacks} />
     </div>
   );
 };
 
-export default ResultsContainer
+export default ResultsContainer;
